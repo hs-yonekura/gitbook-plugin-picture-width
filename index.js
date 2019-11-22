524e16651eabdc7fe28f7b64a78da3156495ae7b
hs-yonekura <y-yonekura@science.co.jp>
2019-11-22 14:33:30 +0900

    all fixed

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-const cheerio = require('cheerio');
-
 module.exports = {
   hooks: {
     'page': function(page) {
-      const $ = cheerio.load(page.content);
-      $('table').each(function(i, table){
-        if (/^{[\s\d\|(px)(em)(pt)%]+}$/g.test($(table).prev().text()))
-        {
-          var widths = $(table).prev().text().match(/[\d(px)(em)(pt)%]+/g);
-          $(table).find('th').each(function(i, th){
-            if (i < widths.length)
-              $(th).attr('width', widths[i]);
-          });
-          $(table).prev().remove();
-        }
-      });
-      page.content = $.html();
+      page.content = page.content.replace(/\{\s*([\d\%(em)(pt)(px)]+)\s*\}(<img.*?)>/gim, '$2 width=\"$1\">');
       return page;
     }
   }
-
 }

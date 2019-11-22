51396549babd5704a66c332bec9b856fbc47be09
hs-yonekura <y-yonekura@science.co.jp>
2019-11-22 15:58:31 +0900

    position function added

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 module.exports = {
   hooks: {
     'page': function(page) {
-      page.content = page.content.replace(/\{\s*([\d\%(em)(pt)(px)]+)\s*\}(<img.*?)>/gim, '$2 width=\"$1\">');
+      var content = page.content;
+      var result = content.match(/\{\s*?([\|\scrl\d\%(em)(pt)(px)]+)\s*\}(<img.*?)>/gmi);
+      if(result !== null)
+      {
+        result.forEach(function(r){
+          var val = r.replace(/<img.*?>/g, '').replace(/[\{\}]/g, '').replace(/\s*\|\s*/g, '|').trim().split('|');
+          var replaceText = r.replace(/{.*?}/, '');
+          val.forEach(function(v){
+            var bl = false;
+            switch(v){
+              case 'c':
+                replaceText = '<div style="text-align:center">' + replaceText + '</div>';
+                break;
+              case 'r':
+                replaceText = '<div style="text-align:right">' + replaceText + '</div>';
+                break;
+              case 'l':
+                replaceText = '<div style="text-align:light">' + replaceText + '</div>';
+                break;
+              default:
+                replaceText = replaceText.replace('<img', '<img width="' + v + '" ');
+                break;
+            }
+          });
+          content = content.replace(r, replaceText);
+        });
+      }
+      page.content = content;
       return page;
     }
   }
